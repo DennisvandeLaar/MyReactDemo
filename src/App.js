4a56cd75d8bf21c911d81ab0e2bb1facda3c228d
DennisvdLaar <dennis.van.de.laar@microsoft.com>
2023-11-11 15:40:24 +0100

    .

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(process.env.REACT_APP_API_URL);
+      const result = await axios('https://dvdlweatherforecast.azurewebsites.net/weatherforecast');
       setData(result.data);
     };
 
